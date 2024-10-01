e356f749b4224a0dbf94aa033bf8d94bba01205c
danylo_firsov <danfirsov.it@gmail.com>
2024-10-01 12:37:30 +0300

    update repo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import "./App.css"
-import Header from "./components/header/Header"
-import Main from "./components/main/Main"
+import Header from "./Components/header/Header"
 
 function App() {
   return (
