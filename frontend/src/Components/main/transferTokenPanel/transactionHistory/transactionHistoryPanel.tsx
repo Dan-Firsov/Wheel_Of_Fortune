f05adc985d1341e449b96eb2a9f5834cd2d383aa
danylo_firsov <danfirsov.it@gmail.com>
2024-10-08 12:54:11 +0300

    fix update history panel

--- frontend/src/Components/main/transferTokenPanel/transactionHistory/transactionHistoryPanel.tsx
+++ frontend/src/Components/main/transferTokenPanel/transactionHistory/transactionHistoryPanel.tsx
@@ -4,13 +4,15 @@ import { tokenAddress, useAddress, useConnection } from "../../../../store/Walle
 import { useEffect, useState } from "react"
 
 export default function TransactionHistoryPanel() {
-  const { signer } = useConnection.getState()
   const { address } = useAddress.getState()
   const [transactions, setTransactions] = useState<any[]>([])
+  console.log(address)
 
   useEffect(() => {
     const fetchTransaction = async () => {
-      if (signer && address) {
+      if (address) {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const signer = await provider.getSigner()
         const contract = new ethers.Contract(tokenAddress, WildTokenABI, signer)
         const filter = contract.filters.Transfer(address, null)
         const events = await contract.queryFilter(filter)
@@ -21,21 +23,19 @@ export default function TransactionHistoryPanel() {
             from: decoded[0],
             to: decoded[1],
             value: ethers.formatUnits(decoded[2], 18),
-            hash: event.transactionHash,
           }
         })
-
-        setTransactions((prev) => [...formattedEvents, ...prev])
+        setTransactions(() => [...formattedEvents])
       }
     }
     fetchTransaction()
-  }, [signer, address])
+  }, [address])
 
   return (
     <div>
       <h3>Transaction History</h3>
       <ul>
-        {transactions.map((tx, index) => (
+        {transactions.reverse().map((tx, index) => (
           <li key={index}>
             From: {tx.from}, To: {tx.to}, Value: {tx.value}
           </li>
