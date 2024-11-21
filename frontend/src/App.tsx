a43db3baadb3a29915f42ccca750662ea47932cf
danylo_firsov <danfirsov.it@gmail.com>
2024-11-21 14:07:50 +0200

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import Footer from "./components/footer/footer"
+import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Main from "./components/main/Main"
 
